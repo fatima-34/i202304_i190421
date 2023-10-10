2a13b0d878d0d1f154104fc68c39dd4b865d1431
fatima-34 <fatimafarooq143072@gmail.com>
2023-10-10 07:02:10 +0500

    updates done

--- app/src/main/java/com/ass2/i202304_i190421/last.java
+++ app/src/main/java/com/ass2/i202304_i190421/last.java
@@ -2,13 +2,24 @@ package com.ass2.i202304_i190421;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class last extends AppCompatActivity {
-
+    private static final long DELAY_MILLIS = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(last.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY_MILLIS);
     }
 }
